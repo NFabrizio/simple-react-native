ba12029e94f2c464ed86881c8f872f5ddaf7e71b
NFabrizio <nick.fabrizio@pearson.com>
2017-07-31 08:41:26 -0600

    Adds ListView to display course data for React Courses

--- SimpleReactNative/src/components/ReactCourses.js
+++ SimpleReactNative/src/components/ReactCourses.js
@@ -1,5 +1,18 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  ListView,
+  Image
+} from 'react-native';
+import data from '../data/courses.json';
+
+const ds = new ListView.DataSource({
+  rowHasChanged: (r1, r2) => r1 !== r2
+});
+const dataSource = ds.cloneWithRows(data);
 
 export default class ReactCourses extends React.Component {
   static navigationOptions = {
@@ -11,12 +24,28 @@ export default class ReactCourses extends React.Component {
 
     return (
       <View style={styles.container}>
-        <Text style={styles.header}>Welcome to React Courses</Text>
         <Button
           onPress={() => navigate('NativeCourses')}
           title='React Native Courses'
           style={styles.button}
         />
+        <ListView
+          dataSource={dataSource}
+          renderRow={(rowData) => {
+            return (
+              <View style={styles.course}>
+                <Text style={styles.courseTitle}>{rowData.title}</Text>
+                <Text>{rowData.description}</Text>
+                <Text>{rowData.views}</Text>
+                <Text>{rowData.link}</Text>
+                <Image
+                  source={{uri: rowData.image}}
+                  style={styles.image}
+                  />
+              </View>
+            );
+          }}
+          />
       </View>
     );
   }
@@ -25,15 +54,28 @@ export default class ReactCourses extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
+    backgroundColor: '#F5FCFF',
+    paddingTop: 10
   },
   header: {
     fontSize: 18,
     fontWeight: 'bold'
   },
+  course: {
+    paddingTop: 10,
+    paddingBottom: 10
+  },
+  courseTitle: {
+    fontSize: 14,
+    fontWeight: 'bold',
+    paddingBottom: 5
+  },
   button: {
     width: 100
+  },
+  image: {
+    width: 400,
+    height: 200,
+    marginTop: 5
   }
 });
